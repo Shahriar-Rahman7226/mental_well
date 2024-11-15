6cca7e59458881176bb2884b72e28acd5c6e4eac
Shahriar Rahman <shahriar.rahman0709@gmail.com>
2024-11-15 20:21:09 +0600

    counselor schedule, appointment request and payment added

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -1,91 +1,70 @@
 from django.db import models
 from abstract.base_model import CustomModel
 from apps.user_profile.models import *
-from external.choice_tuple import Days, AppointmentStatus
+from external.choice_tuple import Days, AdminStatus, ClientOverview, PackageType, PaymentMethodType
 
-
-#Available time slots or schedules for each counselor
+# ----- COUNSELOR MODELS -----
+# Available time slots or schedules for each counselor
 class CounselorSchedule(CustomModel):
     counselor = models.ForeignKey(CounselorProfileModel, related_name='counselor_schedule', on_delete=models.CASCADE, blank=True, null=True)
     day = models.CharField(max_length=100, blank=True, null=True, choices=Days)
     start_time = models.TimeField(blank=True, null=True)
     end_time = models.TimeField(blank=True, null=True)
-    is_available = models.BooleanField(default=True, blank=True, null=True)
-    booked = models.BooleanField(default=False, blank=True, null=True)
-    status = models.CharField(max_length=100, blank=True, null=True)
+    is_available = models.BooleanField(blank=True, null=True, default=True)
+    is_booked = models.BooleanField(blank=True, null=True, default=False)
+    status = models.CharField(max_length=100, blank=True, null=True, choices=AdminStatus, default=AdminStatus[0][0])
 
     class Meta:
         db_table='counselor_schedule'
         ordering=['-created_at']
 
     def __str__(self):
-        return f"{self.counselor.user.first_name if self.counselor.user else ''} {self.counselor.user.last_name if self.counselor.user else ''} - {self.day if self.day else ''}"
-    
-#When clients book appointments with counselors
+        return f"{self.client.user.first_name if self.client.user else ''} {self.client.user.last_name if self.client.user else ''} - {self.day if self.day else ''}"
+
+
+# When clients book appointments with counselors
 class AppointmentRequest(CustomModel):
-    counselor = models.ForeignKey(CounselorProfileModel, related_name='counselor_schedule', on_delete=models.CASCADE, blank=True, null=True)
-    client_email = models.EmailField(blank=True, null=True)
-    client_number = models.CharField(max_length=100, blank=True, null=True)
+    counselor = models.ForeignKey(CounselorProfileModel, related_name='counselor_request', on_delete=models.CASCADE, blank=True, null=True)
+    client = models.ForeignKey(ClientProfileModel, related_name='client_request', on_delete=models.CASCADE, blank=True, null=True)
     schedule = models.ForeignKey(CounselorSchedule, related_name='appointment_schedule', on_delete=models.CASCADE, blank=True, null=True)
     booking_date = models.DateTimeField(blank=True, null=True)
-    status=models.CharField(max_length=100, blank=True, null=True, choices=AppointmentStatus, default='PENDING')
+    status=models.CharField(max_length=100, blank=True, null=True, choices=AdminStatus, default=AdminStatus[0][0])
+    is_paid = models.BooleanField(blank=True, null=True, default=False)
 
     class Meta:
         db_table='appointment_request'
         ordering=['-created_at']
     
     def __str__(self):
-        return f"{self.counselor.user.first_name if self.counselor.user else ''} {self.counselor.user.last_name if self.counselor.user else ''} - {self.client_email if self.client_email else ''}" (Create views for this)
-
-#Session Price and Booking
-class SessionPackage(models.Model):
-    PACKAGE_CHOICES = [
-        ('SINGLE', 'Single Session'),
-        ('FIVE', '5-Session Package'),
-    ]
-    
-    name = models.CharField(max_length=50, choices=PACKAGE_CHOICES, unique=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    sessions_included = models.PositiveIntegerField(default=1)
-
-    class Meta:
-        db_table = 'session_package'
-
-    def __str__(self):
-        return f"{self.get_name_display()} - ${self.price}"
-
-    @staticmethod
-    def create_default_packages():
-        single_session, _ = SessionPackage.objects.get_or_create(
-            name='SINGLE',
-            defaults={'price': 20.00, 'sessions_included': 1}
-        )
-        package_session, _ = SessionPackage.objects.get_or_create(
-            name='FIVE',
-            defaults={'price': 80.00, 'sessions_included': 5}
-        )
+        return f"{self.client.user.first_name if self.client.user else ''} {self.client.user.last_name if self.client.user else ''} - {self.status if self.status else ''}"
 
 
 #Payment Type
-class Payment(models.Model):
-    PAYMENT_METHOD_CHOICES = [
-        ('BKASH', 'bKash'),
-        ('NAGAD', 'Nagad'),
-    ]
-    
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    package = models.ForeignKey(SessionPackage, on_delete=models.CASCADE)
-    counselor = models.ForeignKey(CounselorProfileModel, on_delete=models.SET_NULL, null=True)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateTimeField(auto_now_add=True)
-    is_successful = models.BooleanField(default=False)
+class Payment(CustomModel):
+    counselor = models.ForeignKey(CounselorProfileModel, related_name='counselor_payment', on_delete=models.CASCADE, blank=True, null=True)
+    client = models.ForeignKey(ClientProfileModel, related_name='client_payment', on_delete=models.CASCADE, blank=True, null=True)
+    # package = models.ForeignKey(SessionPackage, on_delete=models.CASCADE)
+    appointment = models.ForeignKey(AppointmentRequest, related_name='appointment_schedule', on_delete=models.CASCADE, blank=True, null=True)
+    due_amount = models.FloatField(blank=True, null=True)
+    paid_amount = models.FloatField(blank=True, null=True)
+    payment_date = models.DateField(blank=True, null=True)
     transaction_id = models.CharField(max_length=255, blank=True, null=True)
-    payment_method = models.CharField(max_length=10, choices=PAYMENT_METHOD_CHOICES)
-    status = models.CharField(max_length=50, default="Pending")
+    payment_method = models.CharField(max_length=100, choices=PaymentMethodType, default=PaymentMethodType[0][0], blank=True, null=True)
 
     class Meta:
         db_table = 'payment'
 
     def __str__(self):
-        return f"{self.user.username} - {self.package.name} - {'Success' if self.is_successful else 'Failed'}"
+        return f"{self.client.user.first_name if self.client.user else ''} {self.client.user.last_name if self.client.user else ''} - {self.transaction_id if self.transaction_id else ''}"
+
+
+# #Session Price and Booking
+# class SessionPackage(CustomModel):
+#     package = models.CharField(max_length=100, choices=PackageType, unique=True, blank=True, null=True)
+#     price = models.FloatField(max_digits=10, decimal_places=2, blank=True, null=True)
+#     sessions_count = models.PositiveIntegerField(default=0, blank=True, null=True)
+
+#     class Meta:
+#         db_table = 'session_package'
+
 
