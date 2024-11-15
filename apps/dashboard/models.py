131127fc9113f8cc106e17608a8a96fab6ea14e7
Shahriar Rahman <shahriar.rahman0709@gmail.com>
2024-11-15 21:40:23 +0600

    bannermodel, motivationalmodel, resources, legaldocument, privacypolicy, aboutus, footermodel, clientprogress, clientprogressdetails, achievements models added

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -5,6 +5,90 @@ from apps.user_profile.models import *
 from external.choice_tuple import Days, AdminStatus, ClientOverview, PackageType, PaymentMethodType, ReviewStatus
 
 
+# ----- GENERAL MODELS -----
+class BannerModel(CustomModel):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    image = models.ImageField(upload_to='banner/', blank=True, null=True)
+
+    class Meta:
+        db_table='banner_model'
+        ordering=['-created_at']
+
+
+class MotivationModel(CustomModel):
+    quote_text = models.TextField(blank=True, null=True)
+    author = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        db_table='motivation_model'
+        ordering=['-created_at']
+
+
+class Resources(CustomModel):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    details = models.TextField(blank=True, null=True)
+    resource_file = models.FileField(blank=True, null=True)
+    resource_link = models.URLField(blank=True, null=True)
+
+    class Meta:
+        db_table='resources'
+        ordering=['-created_at']
+
+
+class LegalDocument(CustomModel):
+    details = models.TextField(blank=True, null=True)
+    version = models.CharField(max_length=100, blank=True, null=True)
+    licence_document = models.FileField(blank=True, null=True)
+
+    class Meta:
+        db_table='legal_document'
+        ordering=['-created_at']
+
+
+class PrivacyPolicy(CustomModel):
+    details = models.TextField(blank=True, null=True)
+
+    class Meta:
+        db_table='privacy_policy'
+        ordering=['-created_at']
+
+
+class AboutUs(CustomModel):
+    introduction = models.TextField(blank=True, null=True)
+    mission_statement = models.TextField(blank=True, null=True)
+    vision_statement = models.TextField(blank=True, null=True)
+    history = models.TextField(blank=True, null=True)
+    details = models.TextField(blank=True, null=True)
+    contact_number = models.CharField(max_length=100, blank=True, null=True)
+    contact_email = models.EmailField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    facebook = models.URLField(blank=True, null=True)
+    instagram = models.URLField(blank=True, null=True)
+    youtube = models.URLField(blank=True, null=True)
+    linkedin = models.URLField(blank=True, null=True)
+
+    class Meta:
+        db_table='about_us'
+        ordering=['-created_at']
+
+
+class FooterModel(CustomModel):
+    image = models.ImageField(upload_to='footer/', blank=True, null=True)
+    details = models.TextField(blank=True, null=True)
+    contact_number = models.CharField(max_length=100, blank=True, null=True)
+    contact_email = models.EmailField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    emergency_support = models.TextField(blasnk=True, null=True)
+    facebook = models.URLField(blank=True, null=True)
+    instagram = models.URLField(blank=True, null=True)
+    youtube = models.URLField(blank=True, null=True)
+    linkedin = models.URLField(blank=True, null=True)
+
+    class Meta:
+        db_table='footer_models'
+        ordering=['-created_at']
+
+
 # ----- COUNSELOR MODELS -----
 # Available time slots or schedules for each counselor
 class CounselorSchedule(CustomModel):
@@ -39,6 +123,40 @@ class AppointmentRequest(CustomModel):
     
     def __str__(self):
         return f"{self.client.user.first_name if self.client.user else ''} {self.client.user.last_name if self.client.user else ''} - {self.status if self.status else ''}"
+
+
+class ClientProgress(CustomModel):
+    counselor = models.ForeignKey(CounselorProfileModel, related_name='client_progress_counselor', on_delete=models.CASCADE, blank=True, null=True)
+    client = models.ForeignKey(ClientProfileModel, related_name='client_progress_client', on_delete=models.CASCADE, blank=True, null=True)
+    status = models.CharField(blank=True, null=True, choices=ClientOverview, default=ClientOverview[0][0])
+    session_count = models.PositiveIntegerField(blank=True, null=True)
+
+    class Meta:
+        db_table='client_progress'
+        ordering=['-created_at']
+
+
+class ClientProgressDetails(CustomModel):
+    overview = models.ForeignKey(ClientOverview, related_name='progress_details', on_delete=models.CASCADE, blank=True, null=True)
+    appointment = models.ForeignKey(AppointmentRequest, related_name='progress_appointment', on_delete=models.CASCADE, blank=True, null=True)
+    details = models.TextField(blank=True, null=True)
+
+    class Meta:
+        db_table='client_progress_details'
+        ordering=['-created_at']
+
+
+class Achievements(CustomModel):
+    counselor = models.ForeignKey(CounselorProfileModel, related_name='counselor_achievements', on_delete=models.CASCADE, blank=True, null=True)
+    title = models.CharField(max_length=100, blank=True, null=True)
+    awarded_by = models.Charfield(max_length=100, blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    details = models.TextField(blank=True, null=True)
+
+    class Meta:
+        db_table='achievements'
+        ordering=['-created_at']
+
 
 class Article(CustomModel):
     thumbnail = models.ImageField(upload_to='article/', blank=True, null=True)
@@ -75,6 +193,19 @@ class VideoJournal(CustomModel):
     class Meta:
         db_table='video_journal'
         ordering = ['-created_at'] 
+
+
+# ----- CLIENT MODELS -----
+class FAQModel(CustomModel):
+    client = models.ForeignKey(UserModel, related_name='faq_client', on_delete=models.CASCADE, blank=True, null=True)
+    question = models.TextField(blank=True, null=True)
+    asnwer = models.TextField(blank=True, null=True)
+    is_published = models.BooleanField(blank=True, nul=True)
+
+    class Meta:
+        db_table='faq_model'
+        ordering=['-created_at']
+
 
 #Payment Type
 class Payment(CustomModel):
@@ -113,6 +244,7 @@ class Review(CustomModel):
     client_name = models.CharField(max_length=100, blank=True, null=True)
     status = models.CharField(max_length=100, blank=True, null=True, choices=ReviewStatus, default=ReviewStatus[0][0])
     is_published = models.BooleanField(blank=True, null=True) 
+    appointment_count = models.PositiveIntegerField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.client_name if self.client_name else ''} - {self.rating if self.rating else ''}"
